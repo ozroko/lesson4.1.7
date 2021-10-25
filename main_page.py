99cab375931c13cda7f5a87fba892516407ea068
ozroko <82396551+ozroko@users.noreply.github.com>
2021-10-25 18:43:23 +0300

    поставили заглушку и перенесли тесты в base_page

---
+++ main_page.py
@@ -0,0 +1,10 @@
+import time
+
+from .base_page import BasePage
+# from selenium.webdriver.common.by import By
+# from .locators import MainPageLocators
+
+
+class MainPage(BasePage):
+    def __init__(self, *args, **kwargs):
+        super(MainPage, self).__init__(*args, **kwargs)
