df60a5b87f673f3cf8f27f43a17553394d3708db
ozroko <82396551+ozroko@users.noreply.github.com>
2021-10-25 18:42:30 +0300

    Add files via upload

---
+++ login_page.py
@@ -0,0 +1,25 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_basket_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert self.url.find("login") > 0, "URL is not corrected"
+
+    def should_be_basket_url(self):
+        assert self.url.find("basket") > 0, "URL is not corrected"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert True
